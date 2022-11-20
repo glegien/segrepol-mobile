902d659f103995cc7af0025ff286402be8a9bc6c
Grzegorz Legień <glegien@gmail.com>
2022-11-20 04:24:10 +0100

    Broken

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:appinio_swiper/appinio_swiper.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:segrepol/chat.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:segrepol/chat.dart';
 import 'package:segrepol/menu_overlay.dart';
 
 import 'init.dart';
@@ -26,13 +23,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Swipe Demo',
+      title: 'Hubka',
       initialRoute: '/',
       routes: {
         '/': (context) => const MyHomePage(
-              title: 'Hubka',
+              title: 'SEGREPOL',
             ),
-        '/chat': (context) => ChatPage(),
+        '/chat': (context) => new ChatPage(),
       },
       theme: ThemeData(
         // This is the theme of your application.
@@ -118,68 +115,48 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  List<Card> buildCards() {
+  List<Card> buildCards(BuildContext context) {
     List<Card> list = List.empty(growable: true);
     for (Trash el in trashList!) {
-      list.add(el.buildCard());
+      list.add(el.buildCard(context));
     }
     return list;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       floatingActionButton: OverlayMenu(),
-      appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-          leading: CircleAvatar(
-            backgroundColor: Colors.brown.shade800,
-            child: FutureBuilder(
-                future: _initFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return Text(Init.deviceId!);
-                  } else {
-                    return const Text("LOADING...");
-                  }
-                }),
-          )),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             SizedBox(
-                height: MediaQuery.of(context).size.height * 0.75,
+                height: MediaQuery.of(context).size.height * 0.08,
+                child: Row(
+                  children: [
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+                    Text(
+                      "SEGREPOL",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: "Anton",
+                          fontSize: MediaQuery.of(context).size.height * 0.07,
+                          shadows: const [
+                            Shadow(color: Colors.black, blurRadius: 4)
+                          ]),
+                    )
+                  ],
+                ) // lower menu
+                ),
+            SizedBox(
+                height: MediaQuery.of(context).size.height * 0.55,
                 child: FutureBuilder(
                     future: _fetchTrashes,
                     builder: (context, snapshot) {
                       if (snapshot.connectionState == ConnectionState.done) {
                         return AppinioSwiper(
-                          cards: buildCards(),
+                          cards: buildCards(context),
                           onSwipe: _swipe,
                           onEnd: () => setState(() {
                             fetchMoreTrashes();
@@ -189,8 +166,6 @@ class _MyHomePageState extends State<MyHomePage> {
                         return const Text("LOADING...");
                       }
                     })),
-            SizedBox(child: Row() // lower menu
-                )
           ],
         ),
       ),
--- lib/model.dart
+++ lib/model.dart
@@ -13,19 +13,37 @@ class Trash {
 
   Trash(this.id, this.name, this.description, this.userId, this.imageStr);
 
-  Card buildCard() {
+  Card buildCard(BuildContext context) {
     return Card(
       child: Column(
+        //mainAxisSize: MainAxisSize.max,
+        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Image(image: Image.memory(base64Decode(imageStr)).image),
           SizedBox(
-              //width: screenSize.width / 1.2,
-              //height: screenSize.height / 1.7 - screenSize.height / 2.2,
+            child: Image(
+              image: Image.memory(base64Decode(imageStr)).image,
+            ),
+            height: MediaQuery.of(context).size.height * 0.50,
+          ),
+          SizedBox(
+              //width: MediaQuery.of(context).size.width * 0.9,
+              //height: MediaQuery.of(context).size.height * 0.25,
               child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Text(name),
-              Text(description),
+              Text(name,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Anton",
+                    //fontSize: MediaQuery.of(context).size.height * 0.04,
+                  )),
+              Text(description,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: "Anton",
+                    // fontSize: MediaQuery.of(context).size.height * 0.03,
+                  )),
             ],
           ))
         ],
@@ -34,10 +52,11 @@ class Trash {
   }
 
   static List<Trash> fromJson(Map<String, dynamic> json) {
-    List<Trash> result = List.empty(growable:true);
+    List<Trash> result = List.empty(growable: true);
     for (var el in json['result']) {
       log(el.toString());
-      result.add(Trash(el['id'], el['name'], el['description'], el['userId'], el['image']));
+      result.add(Trash(
+          el['id'], el['name'], el['description'], el['userId'], el['image']));
     }
     return result;
   }
