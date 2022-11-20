e0e51e7665deb5152cc8cac388c6f95ec6fa23e7
Filip Cygan <filipcygan@outlook.com>
2022-11-20 05:13:59 +0100

    add periodic chat polling and sending message

--- lib/Conservation.dart
+++ lib/Conservation.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:segrepol/init.dart';
 import 'package:segrepol/model/ConservationModel.dart';
 
 class Conservation extends StatefulWidget {
@@ -14,10 +16,56 @@ class Conservation extends StatefulWidget {
 }
 
 class _ConservationState extends State<Conservation> {
+  final titleController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    Timer.periodic(Duration(seconds: 5), (Timer t) => setState((){}));
     return Scaffold(
-      body: _list(),
+      // body: SafeArea(child: _list()),
+      body: Stack(
+        children: [
+          _list(),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                          hintText: "Write message...",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () => sendMessage(),
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    backgroundColor: Colors.green,
+                    elevation: 0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -56,5 +104,22 @@ class _ConservationState extends State<Conservation> {
       },
       future: _loadEmojiAsList(),
     );
+  }
+
+  sendMessage() async {
+    final response = await http.post(
+        Uri.parse(
+            'https://europe-central2-segrepol-b80d8.cloudfunctions.net/sendMessage'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          "chatId": widget.chatId,
+          "message": titleController.text.toString(),
+          "senderId": Init.deviceId!
+        }));
+    if (response.statusCode == 200) {
+      setState(() {});
+    }
   }
 }
