f87348a9c2ad6a0534796efddb6fedc4cab101c8
Grzegorz Legień <glegien@gmail.com>
2022-11-20 01:46:52 +0100

    Reload1

--- lib/init.dart
+++ lib/init.dart
@@ -13,7 +13,7 @@ class Init {
     final prefs = await SharedPreferences.getInstance();
     final String? userId;
     if (prefs.getString("userId") == null) {
-      userId = "#UX${Random().nextInt(99999)}";
+      userId = "UX${Random().nextInt(99999)}";
       prefs.setString("userId", userId);
     } else {
       userId = prefs.getString("userId");
